016d0b086a97822108e7fc789c9986b5ff480626
aborruso <aborruso@gmail.com>
2025-04-12 15:31:56 +0200

    Aggiorna il comando mlr nel file summary.sh per migliorare la generazione del riepilogo degli URL

--- scripts/summary.sh
+++ scripts/summary.sh
@@ -19,4 +19,4 @@ check_mlr_installation
 
 folder="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-mlr --ijsonl --ocsv cut -f link then put '$l=sub($link,"^(http.+//.+?/).+$","\1")' then cut -f l then count -g l then sort -tr count then label fonte "${folder}"/../data/feed_entries.jsonl > "${folder}"/../data/url_summary.csv
+mlr --ijsonl --ocsv cut -f link then put '$l=sub($link,"^(http.+//.+?/).+$","\1")' then cut -f l then count -g l then sort -tr count -f l then label fonte "${folder}"/../data/feed_entries.jsonl > "${folder}"/../data/url_summary.csv
